ec43ff331451ce7a3decaed72816c4cbf9248386
Hildek <tomasz.hildebrand@gmail.com>
2023-01-23 20:28:04 +0100

    zadanie 2d

---
+++ Zadania_trening/Zadania_trening/Licz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania_trening
+{
+    internal class Count
+    {
+        public int Value { get; private set; }
+        public void Add(int input)
+        {
+            Value += input;
+        }
+        public void Subtract(int input)
+        {
+            Value -= input;
+        }
+        public void WriteNumber()
+        {
+            Console.WriteLine("Podaj liczbę do działania: ");
+            Value = int.Parse(Console.ReadLine()!);
+        }
+        public void ReadNumber()
+        { 
+             Console.WriteLine($"Wartość Value: {Value} ");
+        }
+    }
+}
---
+++ Zadania_trening/Zadania_trening/Sumator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania_trening
+{
+    internal class Sumator
+    {
+        private List<int> Numbers { get; set; } = new();
+        private List<int> NumberDevide3 { get; set; } = new();
+        public int sumDevide3;
+        public int amount;
+        public Sumator() 
+        {   
+        }
+
+        public void Sum()
+        {
+            for (int i = 0; i <= Numbers.Count; i++)
+            {
+                 amount += Numbers[i];
+            
+            }
+        }
+        public void SumDevide3()
+        {
+            foreach (int i in Numbers)
+            {
+                if (i % 3 == 0)
+                {
+                    sumDevide3 += i;
+                    NumberDevide3.Add(i);
+                }
+            }
+        }
+        public void HowManyElementsInNumbers()
+        {
+            Console.WriteLine($"Lista ma {Numbers.Count} elemntów ");
+        }
+
+        public void PrintAllElementsFromNumbers() 
+        {
+            foreach (var i in Numbers)
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine("\n");
+        }
+
+    }
+}
